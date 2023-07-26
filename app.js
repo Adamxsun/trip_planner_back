7c26170b42abce5f2ed3809792886f83daa01379
Adamxsun <32780810+Adamxsun@users.noreply.github.com>
2023-07-26 11:59:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+app.use(cors());
 // Sample data for cities
 const cities = [
   { name: 'Rome',  label: '001',description: 'Rome, the capital city of Italy, is a captivating blend of ancient history and modern vibrancy. ' },
